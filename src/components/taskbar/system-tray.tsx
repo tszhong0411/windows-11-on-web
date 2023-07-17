ffb5a35702fcf1c913f8b7c7241e8cf50ed222de
tszhong0411 <lainelson411@gmail.com>
2023-07-17 23:12:50 +0800

    refactor(desktop): divide the code to multiple components

--- src/components/taskbar/system-tray.tsx
+++ src/components/taskbar/system-tray.tsx
@@ -4,7 +4,12 @@ import { cx } from '@tszhong0411/utils'
 import dayjs from 'dayjs'
 import React from 'react'
 
-import { useCalendar, useQuickSettings, useSettings } from '@/hooks'
+import {
+  useCalendar,
+  useClickOutside,
+  useQuickSettings,
+  useSettings,
+} from '@/hooks'
 
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/ui/tooltip'
 import { getVolumeLevel } from '@/utils'
@@ -19,11 +24,21 @@ import {
   WifiIcon,
 } from '../icons'
 const SystemTray = () => {
-  const [time, setTime] = React.useState(new Date())
-  const { volume, setVolume, muted, wifi } = useSettings()
-  const { open: quickSettingsOpen, setOpen: setQuickSettingsOpen } =
-    useQuickSettings()
-  const { open: calendarOpen, setOpen: setCalendarOpen } = useCalendar()
+  return (
+    <div className='flex select-none items-center gap-[3px]'>
+      <QuickSettings />
+      <Clock />
+      <ShowDesktop />
+    </div>
+  )
+}
+
+const QuickSettings = () => {
+  const { volume, setVolume, wifi, muted } = useSettings()
+  const { open, setOpen } = useQuickSettings()
+  const [ref, setRef] = React.useState<HTMLButtonElement | null>(null)
+
+  useClickOutside(() => setOpen(false), [ref])
 
   const adjustVolumeHandler = (e: React.WheelEvent<HTMLDivElement>) => {
     const scrollingUp = e.deltaY < 0
@@ -35,14 +50,6 @@ const SystemTray = () => {
       setVolume(volume - 2)
     }
   }
-
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date())
-    }, 1000)
-
-    return () => clearInterval(interval)
-  }, [])
 
   const VolumeIcon = (props: React.SVGAttributes<SVGElement>) => {
     switch (getVolumeLevel(volume)) {
@@ -60,96 +67,110 @@ const SystemTray = () => {
   }
 
   return (
-    <div className='flex select-none items-center gap-[3px]'>
-      {/* Quick settings */}
-      <button
-        className='flex cursor-default items-center gap-1.5 rounded px-2 text-right text-xs hover:bg-white/90'
-        onClick={() => setQuickSettingsOpen(!quickSettingsOpen)}
-        data-id='quick-settings'
-      >
-        {/* Network */}
-        <Tooltip delayDuration={1000}>
-          <TooltipTrigger asChild>
-            <div className='flex h-10 items-center'>
-              {wifi ? (
-                <WifiIcon width={14} height={14} />
-              ) : (
-                <NoWifiIcon width={14} height={14} />
-              )}
-            </div>
-          </TooltipTrigger>
-          <TooltipContent sideOffset={18}>
-            <span>
-              {wifi ? (
-                <div className='text-left'>
-                  <div>Network</div>
-                  <div>Internet access</div>
-                </div>
-              ) : (
-                <div className='text-left'>
-                  <div>No internet access</div>
-                  <div>No connection available</div>
-                </div>
-              )}
-            </span>
-          </TooltipContent>
-        </Tooltip>
-
-        {/* Volume */}
-        <Tooltip delayDuration={1000}>
-          <TooltipTrigger asChild>
-            <div
-              className='flex h-10 items-center'
-              onWheel={adjustVolumeHandler}
-            >
-              {muted ? (
-                <MutedIcon width={14} height={14} />
-              ) : (
-                <VolumeIcon width={14} height={14} />
-              )}
-            </div>
-          </TooltipTrigger>
-          <TooltipContent sideOffset={18}>
-            <span>
-              Speakers (High Definition Audio Device):{' '}
-              {volume === 0 ? 'Muted' : `${volume}%`}
-            </span>
-          </TooltipContent>
-        </Tooltip>
-      </button>
-
-      {/* Clock */}
+    <button
+      className='flex cursor-default items-center gap-1.5 rounded px-2 text-right text-xs hover:bg-white/70'
+      onClick={() => setOpen(!open)}
+      ref={setRef}
+    >
+      {/* Network */}
       <Tooltip delayDuration={1000}>
         <TooltipTrigger asChild>
-          <button
-            onClick={() => setCalendarOpen(!calendarOpen)}
-            className='cursor-default rounded py-1 pl-2 pr-2 text-right text-xs hover:bg-white/90'
-            data-id='calendar'
-          >
-            <div>{dayjs(time).format('h:mm A')}</div>
-            <div>{dayjs(time).format('M/D/YYYY')}</div>
-          </button>
+          <div className='flex h-10 items-center'>
+            {wifi ? (
+              <WifiIcon width={14} height={14} />
+            ) : (
+              <NoWifiIcon width={14} height={14} />
+            )}
+          </div>
         </TooltipTrigger>
         <TooltipContent sideOffset={18}>
-          <span>{dayjs(time).format('dddd, MMMM D, YYYY')}</span>
+          <span>
+            {wifi ? (
+              <div className='text-left'>
+                <div>Network</div>
+                <div>Internet access</div>
+              </div>
+            ) : (
+              <div className='text-left'>
+                <div>No internet access</div>
+                <div>No connection available</div>
+              </div>
+            )}
+          </span>
         </TooltipContent>
       </Tooltip>
 
-      {/* Show desktop */}
+      {/* Volume */}
       <Tooltip delayDuration={1000}>
         <TooltipTrigger asChild>
-          <div
-            className={cx(
-              'relative ml-[3px] h-12 w-1.5',
-              'hover:before:absolute hover:before:left-0 hover:before:top-1/2 hover:before:block hover:before:h-4 hover:before:w-px hover:before:-translate-y-1/2 hover:before:bg-[#7c8389]'
+          <div className='flex h-10 items-center' onWheel={adjustVolumeHandler}>
+            {muted ? (
+              <MutedIcon width={14} height={14} />
+            ) : (
+              <VolumeIcon width={14} height={14} />
             )}
-          ></div>
+          </div>
         </TooltipTrigger>
-        <TooltipContent sideOffset={12}>
-          <span>Show desktop</span>
+        <TooltipContent sideOffset={18}>
+          <span>
+            Speakers (High Definition Audio Device):{' '}
+            {volume === 0 ? 'Muted' : `${volume}%`}
+          </span>
         </TooltipContent>
       </Tooltip>
-    </div>
+    </button>
+  )
+}
+
+const Clock = () => {
+  const { open, setOpen } = useCalendar()
+  const [time, setTime] = React.useState(new Date())
+  const [ref, setRef] = React.useState<HTMLButtonElement | null>(null)
+
+  useClickOutside(() => setOpen(false), [ref])
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <Tooltip delayDuration={1000}>
+      <TooltipTrigger asChild>
+        <button
+          onClick={() => setOpen(!open)}
+          className='cursor-default rounded py-1 pl-2 pr-2 text-right text-xs hover:bg-white/70'
+          ref={setRef}
+        >
+          <div>{dayjs(time).format('h:mm A')}</div>
+          <div>{dayjs(time).format('M/D/YYYY')}</div>
+        </button>
+      </TooltipTrigger>
+      <TooltipContent sideOffset={18}>
+        <span>{dayjs(time).format('dddd, MMMM D, YYYY')}</span>
+      </TooltipContent>
+    </Tooltip>
+  )
+}
+
+const ShowDesktop = () => {
+  return (
+    <Tooltip delayDuration={1000}>
+      <TooltipTrigger asChild>
+        <div
+          className={cx(
+            'relative ml-[3px] h-12 w-1.5',
+            'hover:before:absolute hover:before:left-0 hover:before:top-1/2 hover:before:block hover:before:h-4 hover:before:w-px hover:before:-translate-y-1/2 hover:before:bg-[#7c8389]'
+          )}
+        ></div>
+      </TooltipTrigger>
+      <TooltipContent sideOffset={12}>
+        <span>Show desktop</span>
+      </TooltipContent>
+    </Tooltip>
   )
 }
 
