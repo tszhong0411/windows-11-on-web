df7287d870cc86c753b3c32d8fb8298cf3bd4603
tszhong0411 <lainelson411@gmail.com>
2023-07-29 18:04:11 +0800

    feat(window): icons on window title bar

---
+++ src/components/icons/chrome-close.tsx
@@ -0,0 +1,17 @@
+export const ChromeCloseIcon = (props: React.SVGAttributes<SVGElement>) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='20'
+      height='20'
+      viewBox='-10 0 2068 2048'
+      fill='currentColor'
+      {...props}
+    >
+      <path
+        fill='currentColor'
+        d='M1024 1169l-849 849q-30 30 -72 30q-44 0 -73.5 -29.5t-29.5 -73.5q0 -42 30 -72l849 -849l-849 -849q-30 -30 -30 -73q0 -21 8 -40t22 -32.5t33 -21.5t40 -8q42 0 72 30l849 849l849 -849q30 -30 73 -30q21 0 39.5 8t32.5 22t22 32.5t8 39.5q0 43 -30 73l-849 849 l849 849q30 30 30 72q0 21 -8 40t-21.5 33t-32.5 22t-40 8q-43 0 -73 -30z'
+      />
+    </svg>
+  )
+}
---
+++ src/components/icons/square.tsx
@@ -0,0 +1,14 @@
+export const SquareIcon = (props: React.SVGAttributes<SVGElement>) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='14'
+      height='14'
+      viewBox='0 0 14 14'
+      fill='currentColor'
+      {...props}
+    >
+      <path d='M0 3C0 1.34315 1.34315 0 3 0H11C12.6569 0 14 1.34315 14 3V11C14 12.6569 12.6569 14 11 14H3C1.34315 14 0 12.6569 0 11V3ZM3 1C1.89543 1 1 1.89543 1 3V11C1 12.1046 1.89543 13 3 13H11C12.1046 13 13 12.1046 13 11V3C13 1.89543 12.1046 1 11 1H3Z' />
+    </svg>
+  )
+}
